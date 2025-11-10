adf4bb99d6b3a36f51908f4da336a56736020925
Abdul Moiz <moiz05366@gmail.com>
2025-11-10 15:26:26 +0500

    updated

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ app.use("/api", routes);
 
 app.use(cors({
     origin: "http://localhost:5173",
-    credentials: true
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"]
 }));
 
 const PORT = ENV.PORT || 5000;
@@ -23,6 +24,10 @@ if (ENV.NODE_ENV !== "production") {
         console.log("server is running.");
     });
 }
+
+// app.listen(PORT, () => {
+//     console.log("server is running.");
+// });
 
 connectDB();
 export default app;
